d147861890554b853b1eed52779da7ae0824e5d1
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2022-10-03 08:04:48 +0530

    Added: Employee Full time salary logic

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -7,10 +7,12 @@ public class EmployeeWageComputation {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		Random random = new Random();
 		int a = random.nextInt(2);
+		int wagePH = 20;
+		int FulldayH = 8;
 		if(a == 1)
-		System.out.println("Employee is Present");
+		System.out.println("Employee is Present\nPer day Wage is: "+(wagePH*FulldayH));
 		else
-			System.out.println("Employee is Absent");
+			System.out.println("Employee is Absent\nPer day Wage is: 0");
 
 	}
 
