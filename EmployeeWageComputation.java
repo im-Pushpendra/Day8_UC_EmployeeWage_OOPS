fa78ba970424527ed6d67020bda80b52c56bcaef
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2022-10-03 07:55:56 +0530

    Employee Wage

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.day8.UC_EmployeeWage_OOPS;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+	}
+
+}
