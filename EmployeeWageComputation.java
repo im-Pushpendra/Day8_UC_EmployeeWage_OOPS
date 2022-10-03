379189727ee87a16d4aa64d12947ca2da937a245
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2022-10-03 09:44:51 +0530

    Added: monthly salary logic

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -6,17 +6,30 @@ public class EmployeeWageComputation {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		Random random = new Random();
-		int a = random.nextInt(3);
+		
 		int wagePH = 20;
 		int FulldayH = 8;
 		int ParttimedayH = 4;
+		int days = 0;
+		int salary = 0;
+		int Monthly_salary = 0;
+		while(days != 20) {
+			days++;
+			int a = random.nextInt(3);
 		switch(a) {
 		case 0: System.out.println("Employee is Absent\nPer day Wage is: 0");
 		break;
-		case 1: System.out.println("Employee is Present\nPer day Wage is: "+(wagePH*FulldayH));
+		case 1: salary = wagePH*FulldayH;
+			System.out.println("Employee is Present Fullday\nPer day Wage is: "+salary);
 		break;
-		case 2: System.out.println("Employee is Present Halfday\nPer day Wage is: "+(wagePH*ParttimedayH));
+		case 2: salary = wagePH*ParttimedayH;
+			System.out.println("Employee is Present Halfday\nPer day Wage is: "+salary);
 		break;
+		}
+		
+		System.out.println();
+		Monthly_salary += salary;
+		System.out.println("Employee's Monthly Salary is: "+Monthly_salary);
 		}
 
 	}
