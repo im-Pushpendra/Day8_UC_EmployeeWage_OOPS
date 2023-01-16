6a295f547db2f606da657eae64d8679d6ddd1822
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2023-01-16 18:18:02 +0530

    Refactor using ArrayList

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,59 +1,59 @@
 package com.bridgelabz.day8.UC_EmployeeWage_OOPS;
 
+import java.util.ArrayList;
+
 public class EmployeeWageComputation implements IEmployeeWage{
 	
-	// Declaring a constant
-		public static final int IS_FULLTIME = 1;
-		public static final int IS_PARTTIME = 2;
-		private int numOfCompany = 0;
-		private CompanyEmpWage[] companyEmpWages;
+	public static final int IS_FULLTIME = 1;
+	public static final int IS_PARTTIME = 2;
 
-		public EmployeeWageComputation() {
-			companyEmpWages = new CompanyEmpWage[10];
-		}
+	private ArrayList<CompanyEmpWage> companyEmpWageList;
 
-		public void addCompany(String companyName, int wagePerHr, int totalWorkingDays, int totalWorkingHrs) {
-			CompanyEmpWage companyEmpWage = new CompanyEmpWage(companyName, wagePerHr, totalWorkingDays, totalWorkingHrs);
-			companyEmpWage.setTotalEmpWage(computeEmployeeWage(wagePerHr, totalWorkingDays, totalWorkingHrs));
-			companyEmpWages[numOfCompany] = companyEmpWage;
-			numOfCompany++;
-		}
+	public EmployeeWageComputation() {
+		companyEmpWageList = new ArrayList<>();
+	}
 
-		public int computeEmployeeWage(int wagePerHr, int totalWorkingDays, int totalWorkingHrs) {
-			int fullTimeHrs = 8;
-			int partTimeHrs = 4;
-			int totalHrs = 0;
-			int days = 0;
-			while (totalHrs < totalWorkingHrs && days < totalWorkingDays) {
-				days++;
-				int isPresent = (int) Math.floor(Math.random() * 10) % 3;
-				switch (isPresent) {
-				case IS_FULLTIME:
-					totalHrs += fullTimeHrs;
-					break;
-				case IS_PARTTIME:
-					totalHrs += partTimeHrs;
-					break;
-				default:
-					totalHrs += 0;
-				}
+	public void addCompany(String companyName, int wagePerHr, int totalWorkingDays, int totalWorkingHrs) {
+		CompanyEmpWage companyEmpWage = new CompanyEmpWage(companyName, wagePerHr, totalWorkingDays, totalWorkingHrs);
+		companyEmpWage.setTotalEmpWage(computeEmployeeWage(wagePerHr, totalWorkingDays, totalWorkingHrs));
+		companyEmpWageList.add(companyEmpWage);
+
+	}
+
+	public int computeEmployeeWage(int wagePerHr, int totalWorkingDays, int totalWorkingHrs) {
+		int fullTimeHrs = 8;
+		int partTimeHrs = 4;
+		int totalHrs = 0;
+		int days = 0;
+		while (totalHrs < totalWorkingHrs && days < totalWorkingDays) {
+			days++;
+			int isPresent = (int) Math.floor(Math.random() * 10) % 3;
+			switch (isPresent) {
+			case IS_FULLTIME:
+				totalHrs += fullTimeHrs;
+				break;
+			case IS_PARTTIME:
+				totalHrs += partTimeHrs;
+				break;
+			default:
+				totalHrs += 0;
 			}
-			return totalHrs * wagePerHr;
+		}
+		return totalHrs * wagePerHr;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+		EmployeeWageComputation employeeWage = new EmployeeWageComputation();
+		employeeWage.addCompany("WIPRO", 8, 8, 30);
+		employeeWage.addCompany("BRIDGELABZ", 12, 8, 40);
+		employeeWage.addCompany("INFOSYS", 18, 90, 30);
+		
+
+		for (CompanyEmpWage cmp : employeeWage.companyEmpWageList) {
+			System.out.println(cmp.getCompanyName() + " : " + cmp.getTotalEmpWage());
 		}
 
-		public static void main(String[] args) {
-			System.out.println("Welcome to Employee Wage Computation Program");
-
-			EmployeeWageComputation employeeWage = new EmployeeWageComputation();
-			employeeWage.addCompany("WIPRO", 8, 8, 30);
-			employeeWage.addCompany("BRIDELABZ", 12, 8, 40);
-			employeeWage.addCompany("INFOSYS", 18, 80, 30);
-			
-
-			for (int i = 0; i < employeeWage.numOfCompany; i++) {
-				System.out.println(employeeWage.companyEmpWages[i].getCompanyName() + " : "
-						+ employeeWage.companyEmpWages[i].getTotalEmpWage());
-			}
-
-		}
+	}
 }
